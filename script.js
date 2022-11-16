a3c32c037aae29d277d44475d5548ea0ca683f4f
lemmoor <49235295+lemmoor@users.noreply.github.com>
2022-11-16 21:57:27 +0100

    added animtions

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const navBtn = document.querySelector('.mobile-burger');
 navBtn.addEventListener("click", () => {
     nav.classList.toggle("mobile-active");
     window.scrollTo({ top: 0, behavior: 'smooth' });
+    navBtn.classList.toggle("close");
 })
 
 addEventListener('resize', () => {
     if(window.innerWidth > 1104){
-        nav.classList.remove("mobile-active")
+        nav.classList.remove("mobile-active");
+        navBtn.classList.remove("close");
     }
     console.log("resize")
 });
